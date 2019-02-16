378782e6715322b588b09abc3b9cb22c49dc4d94
ahmed jamal <a7medjamal@outlook.com>
2019-02-16 22:01:30 +0300

    073 Creating the Users Controller

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,10 @@ namespace DatingApp.API
                    ValidateAudience=false     
                };
            });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(opt => 
+            {
+               opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
