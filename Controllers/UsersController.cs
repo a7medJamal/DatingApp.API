d6eb0e24a2b6b2ca77287d10cfab9c978a84d3d8
ahmed jamal <a7medjamal@outlook.com>
2019-02-17 08:41:57 +0300

    075 Using AutoMapper Part 1

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,33 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingApp.API.Controllers
-{
+namespace DatingApp.API.Controllers {
     [Authorize]
-    [Route("api/[controller]")]
-    public class UsersController :Controller
-    {
+    [Route ("api/[controller]")]
+    public class UsersController : Controller {
         private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
 
-        public UsersController(IDatingRepository repo)
-        {
+        public UsersController (IDatingRepository repo, IMapper mapper) {
+            _mapper = mapper;
             _repo = repo;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
-        {
-            var users =await _repo.GetUsers();
-            return Ok(users);
+        public async Task<IActionResult> GetUsers () {
+            var users = await _repo.GetUsers ();
+
+          var usersReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok (usersReturn);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var user = await _repo.GetUser(id);
-            return Ok (user);
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetUser (int id) {
+            var user = await _repo.GetUser (id);
+
+            var userReturn = _mapper.Map<UserForDetailedDto>(user);
+            return Ok (userReturn);
         }
     }
 }
--- Dtos/UserForDetailedDto.cs
+++ Dtos/UserForDetailedDto.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using DatingApp.API.Models;
 
-namespace DatingApp.API.Data
+namespace DatingApp.API.Dtos
 {
     public class UserForDetailedDto
     {
