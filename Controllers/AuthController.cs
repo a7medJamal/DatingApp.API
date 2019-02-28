87c4e711b90311bb4594308c4dce0117a3b1171c
ahmed jamal <a7medjamal@outlook.com>
2019-02-28 12:33:43 +0300

    114 Adding the main photo to the Nav bar

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -10,75 +11,68 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
-namespace DatingApp.API.Controllers 
-{
-     [Route ("api/[controller]")]
-     [ApiController]
-    public class AuthController : Controller 
-    {
+namespace DatingApp.API.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController (IAuthRepository repo ,IConfiguration config)
-        {
-         _repo = repo;
-         _config = config;
+        private readonly IMapper _mapper;
+        public AuthController (IAuthRepository repo, IConfiguration config, IMapper mapper) {
+            _mapper = mapper;
+            _repo = repo;
+            _config = config;
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
-        {
-            if (!string.IsNullOrEmpty(userForRegisterDto.Username))
-                userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-            
-            if (await _repo.UserExists(userForRegisterDto.Username))
-              ModelState.AddModelError("Username","Username already exists");
+        [HttpPost ("register")]
+        public async Task<IActionResult> Register ([FromBody] UserForRegisterDto userForRegisterDto) {
+            if (!string.IsNullOrEmpty (userForRegisterDto.Username))
+                userForRegisterDto.Username = userForRegisterDto.Username.ToLower ();
+
+            if (await _repo.UserExists (userForRegisterDto.Username))
+                ModelState.AddModelError ("Username", "Username already exists");
 
             // validate request
             if (!ModelState.IsValid)
-             return BadRequest(ModelState);
+                return BadRequest (ModelState);
 
-          
-            var userToCreate =  new User
-            {
-                Username=userForRegisterDto.Username
+            var userToCreate = new User {
+                Username = userForRegisterDto.Username
             };
 
-            var createUser = await _repo.Resister(userToCreate,userForRegisterDto.Password);
+            var createUser = await _repo.Resister (userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
-               
+            return StatusCode (201);
+
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> login([FromBody] UserForLoginDto userForLoginDto)
-        {
-             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),userForLoginDto.Password);
+        [HttpPost ("login")]
+        public async Task<IActionResult> login ([FromBody] UserForLoginDto userForLoginDto) {
+            var userFromRepo = await _repo.Login (userForLoginDto.Username.ToLower (), userForLoginDto.Password);
 
             if (userFromRepo == null)
-            return Unauthorized();
+                return Unauthorized ();
 
             //generate token
-          var tokenHandler = new JwtSecurityTokenHandler();
-          var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
-          var tokenDescriptor = new SecurityTokenDescriptor
-          {
-              Subject = new ClaimsIdentity(new Claim[]
-              {
-               new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
-               new Claim(ClaimTypes.Name, userFromRepo.Username)
-              }),
+            var tokenHandler = new JwtSecurityTokenHandler ();
+            var key = Encoding.ASCII.GetBytes (_config.GetSection ("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity (new Claim[] {
+                new Claim (ClaimTypes.NameIdentifier, userFromRepo.Id.ToString ()),
+                new Claim (ClaimTypes.Name, userFromRepo.Username)
+                }),
 
-              Expires= DateTime.Now.AddDays(1),
-              SigningCredentials=new SigningCredentials(new SymmetricSecurityKey(key), 
-              SecurityAlgorithms.HmacSha512Signature)
-          };
-          var token = tokenHandler.CreateToken(tokenDescriptor);
-          var tokenString= tokenHandler.WriteToken(token);
+                Expires = DateTime.Now.AddDays (1),
+                SigningCredentials = new SigningCredentials (new SymmetricSecurityKey (key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken (tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken (token);
 
-          return Ok (new{tokenString});
-      
-      }
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+                return Ok (new { tokenString, user });
 
+        }
 
     }
 }
