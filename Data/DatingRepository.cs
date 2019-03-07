23f0c493c7d80dc94c0cbcad92d24969900b70af
ahmed jamal <a7medjamal@outlook.com>
2019-03-07 19:58:55 +0300

    145 Sorting results in the API

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -5,62 +5,62 @@ using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DatingApp.API.Data
-{
-    public class DatingRepository : IDatingRepository
-    {
+namespace DatingApp.API.Data {
+    public class DatingRepository : IDatingRepository {
         private readonly DataContext _context;
 
-        public DatingRepository(DataContext context)
-        {
+        public DatingRepository (DataContext context) {
             _context = context;
         }
-        public void Add<T>(T entity) where T : class
-        {
-            _context.Add(entity);
+        public void Add<T> (T entity) where T : class {
+            _context.Add (entity);
         }
 
-        public void Delete<T>(T entity) where T : class
-        {
-            _context.Remove(entity);
+        public void Delete<T> (T entity) where T : class {
+            _context.Remove (entity);
         }
 
-        public Task<Photo> GetMainPhotoForUser(int userId)
-        {
-            return _context.Photos.Where(u=>u.UserId == userId).FirstOrDefaultAsync(p =>p.IsMain);
+        public Task<Photo> GetMainPhotoForUser (int userId) {
+            return _context.Photos.Where (u => u.UserId == userId).FirstOrDefaultAsync (p => p.IsMain);
         }
 
-        public Task<Photo> GetPhoto(int id)
-        {
-            var photo =  _context.Photos.FirstOrDefaultAsync(p=>p.Id == id);
+        public Task<Photo> GetPhoto (int id) {
+            var photo = _context.Photos.FirstOrDefaultAsync (p => p.Id == id);
             return photo;
         }
 
-        public async Task<User> GetUser(int id)
-        {
-            var user = await _context.Users.Include(p=>p.Photos).FirstOrDefaultAsync(u=>u.Id==id);
+        public async Task<User> GetUser (int id) {
+            var user = await _context.Users.Include (p => p.Photos).FirstOrDefaultAsync (u => u.Id == id);
             return user;
         }
 
-        public async Task<PagedList<User>> GetUsers(UserParams userParams)
-        {
-           var users = _context.Users.Include(p=>p.Photos).AsQueryable();
-           
-           users = users.Where(u => u.Id != userParams.UserId);
-           users = users.Where(u => u.Gender == userParams.Gender);
+        public async Task<PagedList<User>> GetUsers (UserParams userParams) {
+            var users = _context.Users.Include (p => p.Photos).OrderByDescending (u => u.LastActive).AsQueryable ();
 
-           if (userParams.MinAge != 18 || userParams.MaxAge !=99)
-           {
-               users =users.Where(u => u.DateofBirth.CalculateAge() >= userParams.MinAge
-               && u.DateofBirth.CalculateAge() <= userParams.MaxAge);
-           }
+            users = users.Where (u => u.Id != userParams.UserId);
+            users = users.Where (u => u.Gender == userParams.Gender);
 
-           return await PagedList<User>.CreateAsync(users,userParams.PageNumber,userParams.PageSize);
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99) {
+                users = users.Where (u => u.DateofBirth.CalculateAge () >= userParams.MinAge &&
+                    u.DateofBirth.CalculateAge () <= userParams.MaxAge);
+            }
+
+            if (!string.IsNullOrEmpty (userParams.orderBy)) {
+                switch (userParams.orderBy) {
+                    case "created":
+                        users = users.OrderByDescending (u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending (u => u.LastActive);
+                        break;
+                }
+            }
+
+            return await PagedList<User>.CreateAsync (users, userParams.PageNumber, userParams.PageSize);
         }
 
-        public async Task<bool> SaveAll()
-        {
-            return await _context.SaveChangesAsync() > 0;
+        public async Task<bool> SaveAll () {
+            return await _context.SaveChangesAsync () > 0;
         }
     }
 }
